1eb04edc47838e79ed32579d9531db0ad05a5e79
konh <konh@yandex.ru>
2018-05-09 00:13:05 +0700

    More useful API;

--- Core/BenchmarkRunner.cs
+++ Core/BenchmarkRunner.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace UnityBenchmarkHarness {
-	class BenchmarkRunner {
+	public class BenchmarkRunner {
 		Stopwatch _timer;
 		long      _startMem;
 
-		public static BenchmarkRunner Start() {
+		public static List<BenchmarkSummary> Run<TArgument>(
+			string name, int iterations, Action<TArgument> payload, params TArgument[] arguments
+		) {
+			var summaries = new List<BenchmarkSummary>();
+			for ( var step = 0; step < arguments.Length; step++ ) {
+				var curArgument = arguments[step];
+				var fullName = $"{name} (iterations: {iterations}, argument: {curArgument})";
+				var results = new List<BenchmarkResult>(iterations);
+				for ( var r = 0; r < iterations; r++ ) {
+					results.Add(Perform(() => payload(curArgument)));
+				}
+				summaries.Add(BenchmarkSummary.CombineResults(fullName, results));
+			}
+			return summaries;
+		}
+
+		static BenchmarkRunner Start() {
 			GC.Collect();
 			return new BenchmarkRunner() {
 				_startMem = GC.GetTotalMemory(false),
@@ -14,13 +31,13 @@ namespace UnityBenchmarkHarness {
 			};
 		}
 
-		public static BenchmarkResult Perform(Action action) {
+		static BenchmarkResult Perform(Action action) {
 			var b = Start();
 			action();
 			return b.Finish();
 		}
 
-		public BenchmarkResult Finish() {
+		BenchmarkResult Finish() {
 			_timer.Stop();
 			var memUsed = GC.GetTotalMemory(false) - _startMem;
 			return new BenchmarkResult(_timer.Elapsed.TotalMilliseconds, memUsed);
--- Core/BenchmarkSummary.cs
+++ Core/BenchmarkSummary.cs
@@ -11,22 +11,7 @@ namespace UnityBenchmarkHarness {
 			Measures = measures;
 		}
 
-		public static List<BenchmarkSummary> TestWrapper<T>(string name, int repeats, Action<T, int> payload, params int[] iterations) where T : new() {
-			var summaries = new List<BenchmarkSummary>();
-			for ( var step = 0; step < iterations.Length; step++ ) {
-				var curIters = iterations[step];
-				var fullName = $"{name} (repeats: {repeats}, iterations: {curIters})";
-				var results = new List<BenchmarkResult>(repeats);
-				for ( var r = 0; r < repeats; r++ ) {
-					var list = new T();
-					results.Add(BenchmarkRunner.Perform(() => payload(list, curIters)));
-				}
-				summaries.Add(CombineResults(fullName, results));
-			}
-			return summaries;
-		}
-
-		static BenchmarkSummary CombineResults(string name, ICollection<BenchmarkResult> results) {
+		internal static BenchmarkSummary CombineResults(string name, ICollection<BenchmarkResult> results) {
 			var measures = new List<BenchmarkMeasure>() {
 				CreateTimeMeause(results),
 				CreateMemMeasure(results)
@@ -34,7 +19,7 @@ namespace UnityBenchmarkHarness {
 			return new BenchmarkSummary(name, measures);
 		}
 
-		static BenchmarkMeasure CreateMeause(string name, Func<BenchmarkResult, double> getter, ICollection<BenchmarkResult> results) {
+		static BenchmarkMeasure CreateMeausure(string name, Func<BenchmarkResult, double> getter, ICollection<BenchmarkResult> results) {
 			double min = double.MaxValue, max = 0, sum = 0;
 			foreach ( var r in results ) {
 				var cur = getter(r);
@@ -51,11 +36,11 @@ namespace UnityBenchmarkHarness {
 		}
 
 		static BenchmarkMeasure CreateTimeMeause(ICollection<BenchmarkResult> results) {
-			return CreateMeause("Time", r => r.Time, results);
+			return CreateMeausure("Time", r => r.Time, results);
 		}
 
 		static BenchmarkMeasure CreateMemMeasure(ICollection<BenchmarkResult> results) {
-			return CreateMeause("Memory", r => r.Memory, results);
+			return CreateMeausure("Memory", r => r.Memory, results);
 		}
 	}
 }
